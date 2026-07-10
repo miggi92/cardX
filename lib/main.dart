dbce78dbd78092743adedf92f95c88690b48daa6
Miguel <miggi92@users.noreply.github.com>
2026-07-10 16:46:56 +0200

    feat: :sparkles: added supabase and auth

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,20 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'core/theme/app_theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/providers/storage_provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
+  await Supabase.initialize(
+    url: 'https://xzjdcoadvmrprdjlrzwc.supabase.co',
+    publishableKey: 'sb_publishable_88t6XwiWV3auqXQLVgWbhw_H46ODCw3',
+  );
+
+  final supabase = Supabase.instance.client;
+  if (supabase.auth.currentUser == null) {
+    await supabase.auth.signInAnonymously();
+  }
 
   runApp(
     ProviderScope(
